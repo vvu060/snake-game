d2594cb591421a2c803c6601a61b79e0e72a5011
vvu060 <vvu060@gmail.com>
2023-09-07 16:25:32 +0530

    use headless ui modal for displaying login ui

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import { SignIn } from '@clerk/clerk-react';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
 
-const Modal = () => {
-  return <div>Modal</div>;
-};
+export default function Modal() {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Modal;
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className=''>
+        <button
+          type='button'
+          onClick={openModal}
+          className='rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'
+        >
+          Sign In
+        </button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as='div' className='relative z-10' onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-purple-200 bg-opacity-25' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel
+                  className={`w-full max-w-lg h-96 transform overflow-hidden rounded-2xl bg-purple-800 p-6 text-left align-middle shadow-xl transition-all flex items-center justify-center`}
+                >
+                  <SignIn />
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
