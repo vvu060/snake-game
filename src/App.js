58541dc65185c2902ccb33ea71d3823a79362876
vvu060 <vvu060@gmail.com>
2023-09-07 16:09:50 +0530

    define grid size, initial snake pos & all required states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+
 function App() {
+  const gridSize = 20;
+
+  const initSnakePosition = [
+    { x: gridSize / 2, y: gridSize / 2 },
+    { x: gridSize / 2, y: gridSize / 2 + 1 },
+  ];
+
+  // define all the states that are needed for the game like score, food, snake, direction, game speed, game started, game over etc
+  const [score, setScore] = useState(0);
+  const [food, setFood] = useState({ x: 9, y: 7 });
+  const [snake, setSnake] = useState(initSnakePosition);
+  const [direction, setDirection] = useState('RIGHT');
+  const [gameSpeed, setGameSpeed] = useState(250);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
+
   return <div className=''>Snake game</div>;
 }
 
