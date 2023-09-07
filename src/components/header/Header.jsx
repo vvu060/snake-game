f8c44fb852fd580eade3fe0af206694975b40075
vvu060 <vvu060@gmail.com>
2023-09-07 16:15:47 +0530

    add game logo in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='h-16 w-full fixed top-0 bg-purple-950 z-10 flex items-center justify-between px-20'>
+      {/* Game Logo */}
+      <img
+        src='https://img.freepik.com/premium-vector/purple-snake-mascot-gaming-logo-template-esports-streamer-facebook-youtube_78159-156.jpg?w=2000'
+        alt=''
+        className='h-12 w-12 rounded-full object-contain'
+      />
+    </div>
+  );
 };
 
 export default Header;
