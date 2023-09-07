e17a5db5932a110eabc45475f41160474bf9e9e7
vvu060 <vvu060@gmail.com>
2023-09-07 16:23:24 +0530

    show image or sign in button based on user presence

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,6 +11,22 @@ const Header = () => {
         alt=''
         className='h-12 w-12 rounded-full object-contain'
       />
+
+      {/* User Info/Sign in button */}
+      {isSignedIn ? (
+        <div className='flex items-center space-x-2'>
+          <img
+            src={user.imageUrl}
+            alt=''
+            className='h-12 w-12 rounded-full object-contain'
+          />
+          <div className='rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
+            <SignOutButton />
+          </div>
+        </div>
+      ) : (
+        <button>Sign in</button>
+      )}
     </div>
   );
 };
